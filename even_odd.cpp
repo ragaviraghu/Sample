4aed7167d1726f7794aa65f5962ed1c63cd05d9f
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-02-20 07:42:34 -0800

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n;
+cout<<"Enter the number:"<<endl;
+cin>>n;
+if(n%2==0)
+{cout<<" The given number "<<n<<" is even"<<endl;
+}
+else
+{cout<<" The given number "<<n<<" is odd"<<endl;
+}
+return 0;
+}
