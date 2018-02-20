bea9020917673a9bbb40d58a53aff48a9aa5d158
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-02-20 08:52:55 -0800

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n;
+cout<<"Enter number of times to print : "<<endl;
+cin>>n;
+for(int i=1;i<=n;i++)
+{cout<<i<<". Hello !"<<endl;
+}
+return 0;
+}
